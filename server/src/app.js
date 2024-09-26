fe5d1927c0c3b770c332183304057c865ddbc2bd
Huzaifa475 <hskhan4759@gmail.com>
2024-09-26 13:49:13 +0530

    Render

--- server/src/app.js
+++ server/src/app.js
@@ -46,6 +46,12 @@ app.use("/api/v1/alert", alertRouter);
 //     });
 // });
 
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
+
 app.use('/', () => {
     console.log('Welcome to server');
 })
