014fd433e9e803fe052fb3fecdc6b224f7710ad9
Huzaifa475 <hskhan4759@gmail.com>
2024-09-26 13:58:17 +0530

    Render

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000/api',
+        target: 'https://societyproblem-server.onrender.com/api',
         changeOrigin: true,
         rewrite: path => path.replace(/^\/api/, '')
       }
